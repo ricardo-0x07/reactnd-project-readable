5d79a7ad828aff163d9d34cacce2eeb3118fb00d
Clive R. Cadogan <ricardo_0x07@yahoo.com>
2017-12-24 14:56:18 -0400

    fix: deployment issue

--- api-server/server.js
+++ api-server/server.js
@@ -7,14 +7,17 @@ const config = require('./config')
 const categories = require('./categories')
 const posts = require('./posts')
 const comments = require('./comments')
+const path = require('path');
 
 const app = express()
 
-app.use(express.static('public'))
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+// app.use(express.static('public'))
 app.use(cors())
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   const help = `
   <pre>
     Welcome to the Udacity Readable API!
@@ -126,7 +129,7 @@ app.use((req, res, next) => {
 })
 
 
-app.get('/categories', (req, res) => {
+app.get('/api/categories', (req, res) => {
     categories.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -139,7 +142,7 @@ app.get('/categories', (req, res) => {
       )
 })
 
-app.get('/:category/posts', (req, res) => {
+app.get('/api/:category/posts', (req, res) => {
     posts.getByCategory(req.token, req.params.category)
       .then(
           (data) => res.send(data),
@@ -152,7 +155,7 @@ app.get('/:category/posts', (req, res) => {
       )
 })
 
-app.get('/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
     posts.getAll(req.token)
       .then(
           (data) => res.send(data),
@@ -165,7 +168,7 @@ app.get('/posts', (req, res) => {
       )
 })
 
-app.post('/posts', bodyParser.json(), (req, res) => {
+app.post('/api/posts', bodyParser.json(), (req, res) => {
     posts.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -178,7 +181,7 @@ app.post('/posts', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id', (req, res) => {
+app.get('/api/posts/:id', (req, res) => {
     posts.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -191,7 +194,7 @@ app.get('/posts/:id', (req, res) => {
       )
 })
 
-app.delete('/posts/:id', (req, res) => {
+app.delete('/api/posts/:id', (req, res) => {
     posts.disable(req.token, req.params.id)
       .then(post => comments.disableByParent(req.token, post))
       .then(
@@ -205,7 +208,7 @@ app.delete('/posts/:id', (req, res) => {
       )
 })
 
-app.post('/posts/:id', bodyParser.json(), (req, res) => {
+app.post('/api/posts/:id', bodyParser.json(), (req, res) => {
     const { option } = req.body
     const id = req.params.id
     posts.vote(req.token, id, option)
@@ -220,7 +223,7 @@ app.post('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.put('/posts/:id', bodyParser.json(), (req, res) => {
+app.put('/api/posts/:id', bodyParser.json(), (req, res) => {
     posts.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -233,7 +236,7 @@ app.put('/posts/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get('/api/posts/:id/comments', (req, res) => {
     comments.getByParent(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -246,7 +249,7 @@ app.get('/posts/:id/comments', (req, res) => {
       )
 })
 
-app.get('/comments/:id', (req, res) => {
+app.get('/api/comments/:id', (req, res) => {
     comments.get(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -259,7 +262,7 @@ app.get('/comments/:id', (req, res) => {
       )
 })
 
-app.put('/comments/:id', bodyParser.json(), (req, res) => {
+app.put('/api/comments/:id', bodyParser.json(), (req, res) => {
     comments.edit(req.token, req.params.id, req.body)
       .then(
         (data) => res.send(data),
@@ -272,7 +275,7 @@ app.put('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments', bodyParser.json(), (req, res) => {
+app.post('/api/comments', bodyParser.json(), (req, res) => {
     comments.add(req.token, req.body)
       .then(
           (data) => res.send(data),
@@ -285,7 +288,7 @@ app.post('/comments', bodyParser.json(), (req, res) => {
       )
 })
 
-app.post('/comments/:id', bodyParser.json(), (req, res) => {
+app.post('/api/comments/:id', bodyParser.json(), (req, res) => {
     const { option } = req.body
     comments.vote(req.token, req.params.id, option)
       .then(
@@ -299,7 +302,7 @@ app.post('/comments/:id', bodyParser.json(), (req, res) => {
       )
 })
 
-app.delete('/comments/:id', (req, res) => {
+app.delete('/api/comments/:id', (req, res) => {
     comments.disable(req.token, req.params.id)
       .then(
           (data) => res.send(data),
@@ -312,6 +315,11 @@ app.delete('/comments/:id', (req, res) => {
       )
 })
 
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  });
+  
 app.listen(config.port, () => {
   console.log('Server listening on port %s, Ctrl+C to stop', config.port)
 })
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 
-const api = "http://localhost:3001"
+const api = "http://localhost:3001/api"
 
 
 // Generate a unique token for storing your bookshelf data on the backend server.
